b2084964551db315ac58b0ef1e9607984efe7618
sebxdixz <sdiazdelafuente9@gmail.com>
2022-06-01 14:37:49 -0400

    No mucho mas que la ultima vez

--- main.py
+++ main.py
@@ -7,11 +7,21 @@
 import sys
 import numpy as np
 
+#((1+len(open('input.txt','r').read()))/2)**(0.5)
 
-def main(n,m,laberinto):
-    laberinto = np.zeros((n, m), dtype=int)
-
+def main(laberinto):
+    laberinto = np.zeros(int(((1+len(open('input.txt','r').read()))/2)**(0.5), int((1+len(open('input.txt','r').read()))/2)**(0.5)), dtype=int)
     for x in open('input.txt','r').read():
-    
-
-  
+        n,m=0,0
+        if x == "1":
+            laberinto[n,m] = 1
+            n =+1
+        if x == "0":
+            laberinto[n,m] = 0
+            n =+1
+        if x == "\n":
+            m =+1
+            n = 0
+    print(laberinto)
+x=0
+main(x)
